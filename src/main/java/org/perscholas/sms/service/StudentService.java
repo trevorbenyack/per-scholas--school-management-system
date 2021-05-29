3ef3c86adaf58ee750fcd7e4bfe87d16f1f2c8bc
Trevor Benyack <trevorbenyack@gmail.com>
2021-05-29 11:45:29 -0400

    changes template to a template file, and courses integrates the template

--- src/main/java/org/perscholas/sms/service/StudentService.java
+++ src/main/java/org/perscholas/sms/service/StudentService.java
@@ -5,6 +5,8 @@ import org.perscholas.sms.dao.StudentRepository;
 import org.perscholas.sms.entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,9 @@ public class StudentService {
 
     StudentRepository studentRepository;
 
+    // This annotation was put here to try to solve a LazyInitializationException
+    // Didn't help... but gonna leave it for now.
+    @Transactional(propagation= Propagation.REQUIRED, readOnly=true, noRollbackFor=Exception.class)
     public Student getStudentById(Long studentId) {
         return studentRepository.getById(studentId);
     }
